f68b52c0055c36f25e6d53dca2f8278b2036a16a
Vicente Damasceno <103980154+Vicente-MD@users.noreply.github.com>
2022-10-13 14:35:34 -0300

    Delete springboot-crud-demo-master directory

--- springboot-crud-demo-master/src/main/java/com/ensat/repositories/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ensat.repositories;
-
-import com.ensat.entities.Product;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-
-}
